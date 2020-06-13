ebe2f7e21e06f0c7c5babeab8554f3f1a3732815
JulienVanDerNiet <julienvanderniet4@gmail.com>
2020-06-13 17:21:03 +0200

    This is the only push bad practice I know :)

---
+++ cmake-build-debug/CMakeFiles/Basic_C_Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Basic_C_Calculator.exe"
+  "Basic_C_Calculator.exe.manifest"
+  "Basic_C_Calculator.pdb"
+  "CMakeFiles/Basic_C_Calculator.dir/main.c.obj"
+  "libBasic_C_Calculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Basic_C_Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    char option;
+    double val1,val2;
+    printf("Enter the option of what you want to do with your numbers \n     - * for multiply \n     - + for addition \n     - - for subtract \n     - / for division \n     - ^ for factorial\n");
+    scanf("\n%c" ,&option);
+    printf("You chose %c\n", option);
+
+    if (option == '*'){
+        printf("Enter the multiplicand and then the multiplier!");
+        scanf("%lf %lf", &val1, &val2);
+        printf("%lf * %lf = %lf",val1,val2,(val1 * val2));
+    }
+    else if(option == '+'){
+        printf("Enter a addend and then another addend!");
+        scanf("%lf %lf", &val1, &val2);
+        printf("%lf + %lf = %lf",val1,val2,(val1 + val2));
+    }
+    else if (option == '-'){
+        printf("Enter the minuend and then the subtrahend!");
+        scanf("%lf %lf", &val1, &val2);
+        printf("%lf - %lf = %lf",val1,val2,(val1 - val2));
+    }
+    else if(option == '/'){
+        printf("Enter the dividend and then the divisor!");
+        scanf("%lf %lf", &val1, &val2);
+        printf("%lf / %lf = %lf",val1,val2,(val1 / val2));
+    }
+    else if(option == '^'){
+        printf("Enter the base number and then the exponent!");
+        scanf("%lf %lf", &val1, &val2);
+        printf("%lf^%lf = %lf",val1,val2,(pow(val1,val2)));
+    }
+}
